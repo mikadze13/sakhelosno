5eb231f56b3a04605b1373a62ae16b172a023f91
Vazha Mikadze <74679350+mikadze13@users.noreply.github.com>
2024-07-02 19:04:54 +0400

    update owl carousel

--- content/js/main.js
+++ content/js/main.js
@@ -2,35 +2,25 @@ $(document).ready(function () {
 
     $("#owl-demo").owlCarousel({
 
-        nav: true,
-        slideSpeed: 300,
-        paginationSpeed: 400,
-        autoplay: true, // Enable autoplay
-        autoplayTimeout: 2000,
-        items: 1,
-
+        // nav: true,
+        // slideSpeed: 300,
+        // paginationSpeed: 400,
+        // autoplay: true, // Enable autoplay
+        // autoplayTimeout: 2000,
+        // items: 1,
+        // rewind: true,
+        // autoplayHoverPause: true,
         // itemsDesktop : false,
         // itemsDesktopSmall : false,
         // itemsTablet: false,
         // itemsMobile : false
-
-    });
-    $("#main__popular-games-container-carousel").owlCarousel({
+        items: 1,
         loop: true,
-        dots: false,
-        dots: false,
-        items: 5,
-        responsive: {
-            0: {
-                items: 2,
-            },
-            578: {
-                items: 3,
-            },
-            1000: {
-                items: 5,
-            }
-        }
+        nav: false,
+        center: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        dots: true,
 
-    });
+    }); 
 });
