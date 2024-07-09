23cafaa7c15375ad6e5079b6f4021f163cf9d1fb
Vazha Mikadze <74679350+mikadze13@users.noreply.github.com>
2024-07-09 10:29:11 +0400

    update

--- content/js/translate.js
+++ content/js/translate.js
@@ -1,28 +1,44 @@
 const translation = {
-    en:{
-        home:"Home",
-
+    ge: {
+        home: "მთავარი",
+        services: "სერვისები",
+        aboutUs: "ჩვენს შესახებ",
+        contact: "კონტაქტი"
     },
-    ge:{
-        home:"მთავარი"
-    }
+    en: {
+        home: "Home",
+        services: "services",
+        aboutUs: "About us",
+        contact: "contact"
+    },
+
 }
 const languageSwitch = document.getElementById("languageSwitch");
 const Home = document.getElementById("Home")
+const service = document.getElementById("service")
+const aboutUs = document.getElementById("aboutUs")
+const contact = document.getElementById("contact")
 // languageSwitch.addEventListener("click",(event)=>{
 //     setLanguage(event.target.value);  
 // })
-const setLanguage = (language)=>{
-    if(language === "ge"){
+const setLanguage = (language) => {
+    if (language === "ge") {
         Home.innerText = translation.ge.home
-    }else if(language === "en"){
+        service.innerText = translation.ge.services
+        aboutUs.innerText = translation.ge.aboutUs
+        contact.innerText = translation.ge.contact
+
+    } else if (language === "en") {
         Home.innerText = translation.en.home
+        service.innerText = translation.en.services
+        aboutUs.innerText = translation.en.aboutUs
+        contact.innerText = translation.en.contact
     }
 }
 languageSwitch.addEventListener("change", () => {
     if (languageSwitch.checked) {
-        setLanguage("ge");
-    } else {
         setLanguage("en");
+    } else {
+        setLanguage("ge");
     }
 });
